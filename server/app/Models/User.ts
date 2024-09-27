d338f232f608fafe50fe4182019da04a25bf5397
macBook08 <hawrylukquentin@pc-233.home>
2024-09-27 22:09:23 +0200

    enlever models

--- server/app/Models/User.ts
+++ server/app/Models/User.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, belongsTo, BelongsTo, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo, BelongsTo, HasOne,hasOne } from '@ioc:Adonis/Lucid/Orm'
 import Role from './Role'
 import Profile from './Profile'
 
@@ -13,7 +13,7 @@ export default class User extends BaseModel {
   @column()
   public email: string
 
-  @column({ serializeAs: null })
+  @column({serializeAs: null})
   public password: string
 
   @column()
@@ -22,57 +22,15 @@ export default class User extends BaseModel {
   @column()
   public status: string
 
-  // New fields based on your specifications
-  @column()
-  public address: string
-
-  @column()
-  public birthDate: DateTime
-
-  @column()
-  public country: string
-
-  @column()
-  public city: string
-
-  @column()
-  public postalCode: string
-
-  @column()
-  public phoneNumber: string
-
-  @column()
-  public username: string
-
-  @column()
-  public subscription: string
-
-  @column()
-  public subscriptionType: string
-
-  @column()
-  public subscriptionStartDate: DateTime
-
-  @column()
-  public subscriptionEndDate: DateTime
-
-  @column()
-  public isEmailVerified: boolean
-
-  @column()
-  public preferences: string
-
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
-  // Relationship with Role
   @belongsTo(() => Role)
   public role: BelongsTo<typeof Role>
 
-  // Relationship with Profile
   @hasOne(() => Profile)
   public profile: HasOne<typeof Profile>
 }
